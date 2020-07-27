dfd3e1a4ee336ba5139be6b3e96a54ff29ccd3f6
Luk1Pr0 <lpytel16@gmail.com>
2020-07-27 21:56:09 +0100

    Updated and completed frontend of the frbrain

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,9 @@ const Logo = () => {
 	return(
 		<div className="ma4 mt0">
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 40 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner"> <img alt="logo" src={ brain }/> </div>
+				<div className="Tilt-inner">
+					<img alt="logo" src={ brain }/>
+				</div>
 			</Tilt>
 		</div>
 	)
